3f7cbbd3e35c6733fcfe63be41d22e05e0f59714
hanhdung123 <94171909+Hanhdung007@users.noreply.github.com>
2023-02-11 16:26:41 +0700

    Initial Release

---
+++ Eproject3/Eproject3/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eproject3.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
