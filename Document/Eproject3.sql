5a7acc7218794e99bd0537fb6f004f33916566db
hanhdung123 <94171909+Hanhdung007@users.noreply.github.com>
2023-02-15 17:20:54 +0700

    Eproject Database

---
+++ Document/Eproject3.sql
@@ -0,0 +1,75 @@
+USE eProject3;
+GO 
+
+create table Labs
+(
+	Labs_ID int primary key identity,
+	LabsName varchar(max)
+)
+go
+
+create table Suppliers
+(
+	Supplier_ID int primary key identity,
+	SupplierName varchar(max) not null
+)
+go 
+CREATE TABLE Devices (
+Devices_ID varchar(50) PRIMARY KEY,
+DeviceName NVARCHAR(MAX) NOT NULL,
+DeviceType NVARCHAR(MAX) NULL,
+BuyFrom NVARCHAR(MAX) NULL,
+Status NVARCHAR(MAX) NULL,
+DateMaintance DATETIME NULL,
+DeviceImg NVARCHAR(MAX) NULL,
+Labs_ID int not null,
+Supplier_ID int not null,
+CONSTRAINT fk_labs_Labs_ID
+FOREIGN KEY (Labs_ID)
+REFERENCES Labs (Labs_ID), 
+CONSTRAINT fk_supplier_Supplier_ID
+FOREIGN KEY (Supplier_ID)
+REFERENCES Suppliers (Supplier_ID),
+);
+go 
+CREATE TABLE Admins (
+ID nvarchar(50) PRIMARY KEY,
+AdminName NVARCHAR(MAX) NOT NULL,
+Password NVARCHAR(MAX) NOT NULL
+);
+go
+create table users
+(
+	Users_ID int primary key identity,
+	Username nvarchar(max) not null,
+	Passwords nvarchar(max) not null,
+	Email nvarchar(max) not null
+)
+go
+create table Complain
+(
+	Complain_ID int primary key identity,
+	description nvarchar(max) not null,
+	Users_ID int not null,
+	CONSTRAINT fk_users_Users_ID
+	FOREIGN KEY (Users_ID)
+	REFERENCES users (Users_ID)
+)
+go
+create table report 
+(
+	Report_ID int primary key identity,
+	Descriptions varchar(max) null,
+	ReportDate datetime not null,
+	Reciver varchar(max) not null,
+	Complain_ID int not null,
+	Devices_ID varchar(50) not null,
+	CONSTRAINT fk_report_Report_ID
+	FOREIGN KEY (Complain_ID)
+	REFERENCES Complain (Complain_ID),
+	CONSTRAINT fk_Devices_Devices_ID
+	FOREIGN KEY (Devices_ID)
+	REFERENCES Devices (Devices_ID)
+)
+go 
+
