abbcd80f4b5112d9e81068e7f7e1ac238dae6c3f
hanhdung123 <94171909+Hanhdung007@users.noreply.github.com>
2023-02-24 16:29:24 +0700

    commit

--- Eproject3/Eproject3/Models/Admin.cs
+++ Eproject3/Eproject3/Models/Admin.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Eproject3.Models
 {
     public partial class Admin
     {
-        public string Id { get; set; } = null!;
-        public string AdminName { get; set; } = null!;
-        public string Password { get; set; } = null!;
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string ID { get; set; }
+        public string AdminName { get; set; }    
+        public string Password { get; set; }
+        public string Role { get; set; } 
     }
 }
--- Eproject3/Eproject3/Models/Device.cs
+++ Eproject3/Eproject3/Models/Device.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Eproject3.Models
 {
@@ -9,19 +11,18 @@ namespace Eproject3.Models
         {
             Reports = new HashSet<Report>();
         }
-
-        public string DevicesId { get; set; } = null!;
-        public string DeviceName { get; set; } = null!;
-        public string DeviceType { get; set; } = null!;
-        public string SupplyFrom { get; set; } = null!;
-        public string? Status { get; set; }
-        public string DateMaintance { get; set; } = null!;
-        public string? DeviceImg { get; set; }
+        [Key]
+        public string DevicesId { get; set; } 
+        public string DeviceName { get; set; } 
+        public string DeviceType { get; set; }
+        public string Status { get; set; }
+        public string DateMaintance { get; set; }
+        public string DeviceImg { get; set; }
         public int LabsId { get; set; }
-        public int SupplierId { get; set; }
+        public int Supplier_ID { get; set; }
 
-        public virtual Lab Labs { get; set; } = null!;
-        public virtual Supplier Supplier { get; set; } = null!;
+        public virtual Lab Labs { get; set; } 
+        public virtual Supplier Supplier { get; set; } 
         public virtual ICollection<Report> Reports { get; set; }
     }
 }
