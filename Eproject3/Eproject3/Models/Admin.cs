99ac6824688e292cb13beedeb23df6843d81c770
Trong Nguyen <trantrong987@gmail.com>
2023-02-25 09:21:18 +0700

    comit login

--- Eproject3/Eproject3/Models/Admin.cs
+++ Eproject3/Eproject3/Models/Admin.cs
@@ -8,5 +8,6 @@ namespace Eproject3.Models
         public string Id { get; set; } = null!;
         public string AdminName { get; set; } = null!;
         public string Password { get; set; } = null!;
+        public string Role { get; set; } = null!;
     }
 }
--- Eproject3/Eproject3/Program.cs
+++ Eproject3/Eproject3/Program.cs
@@ -1,11 +1,25 @@
 using Eproject3.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<eProject3Context>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectDb")));
+builder.Services.AddAuthentication(o =>
+{
+    o.DefaultScheme = Eproject3.Controllers.SchemesNamesConst.TokenAuthenticationDefaultScheme;
+    o.DefaultScheme = Eproject3.Controllers.SchemesNameConst.TokenAuthenticationDefaultScheme;
+});
+builder.Services.AddSession();
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"));
+    options.AddPolicy("StaffOnly", policy => policy.RequireRole("Staff"));
+    options.AddPolicy("UserOnly",  policy => policy.RequireRole("User"));
+});
 
 var app = builder.Build();
 
@@ -18,10 +32,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseSession();
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Account}/{action=Login}/{id?}");
 
 app.Run();
