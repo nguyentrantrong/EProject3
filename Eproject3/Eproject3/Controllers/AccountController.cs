4ffa799849247f2a026ee59cf8075271158640a7
Trong Nguyen <trantrong987@gmail.com>
2023-02-26 11:48:22 +0700

    1

--- Eproject3/Eproject3/Controllers/AccountController.cs
+++ Eproject3/Eproject3/Controllers/AccountController.cs
@@ -3,6 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 using Eproject3.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
 
 namespace Eproject3.Controllers
 {
@@ -14,9 +19,11 @@ namespace Eproject3.Controllers
     public class AccountController : Controller
     {
         private eProject3Context db;
-        public AccountController(eProject3Context _db)
+        private readonly IConfiguration _config;
+        public AccountController(eProject3Context _db, IConfiguration config)
         {
             db = _db;
+            _config = config;
         }
 
         [HttpGet]
@@ -28,57 +35,44 @@ namespace Eproject3.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(string uname, string password)
         {
-            try
+            var user = await db.Admins.SingleOrDefaultAsync(u => u.AdminName.Equals(uname));
+            if (user != null && user.Password.Equals(password))
             {
-                var user = await db.Admins.SingleOrDefaultAsync(u => u.AdminName.Equals(uname));
-                if (user != null && user.Password.Equals(password))
-                {
-                    HttpContext.Session.SetString("adminName", user.AdminName);
-                    return RedirectToAction("Index","Home");
-                }
-                else
-                {
-                    ViewBag.msg = "USERNAME OR PASSWORD IS INCORRECT! PLEASE TRY AGAIN!";
-                }
+                var token = GenerateJwtToken(user);
+
+                // Save token in cookie
+                Response.Cookies.Append("jwtToken", token, new CookieOptions { HttpOnly = true, Secure = true, SameSite = SameSiteMode.None });
+                return RedirectToAction("Index", "Home");
             }
-            catch (Exception ex)
+            else
             {
-                ViewBag.msg = ex.Message;
+                ViewBag.msg = "USERNAME OR PASSWORD IS INCORRECT! PLEASE TRY AGAIN!";
             }
             return View();
         }
 
         public IActionResult Logout()
         {
-            HttpContext.Session.Remove("adminName");
-                
+            // Delete token in cookie
+            HttpContext.Response.Cookies.Delete("jwtToken");
             return RedirectToAction("Login", "Account");
         }
 
-        [Authorize(Policy = "AdminOnly, StaffOnly, UserOnly")]
-        public IActionResult Layout(Admin user)
+        public IActionResult Index()
         {
-            if (HttpContext.Session.GetString("adminName") == null)
-            {
-                return RedirectToAction("Login");
-            }
-            else
-            {
                 var res = db.Admins.ToList();
                 return View(res);
-            }
         }
 
         [HttpGet]
-        [Authorize(Policy = "AdminOnly")]
+        [Authorize(Roles = "admin")]
         public IActionResult Create()
         {
-
             return View();
         }
 
         [HttpPost]
-        [Authorize(Policy = "AdminOnly")]
+        [Authorize(Roles = "admin")]
         public IActionResult Create(Admin newAdmin)
         {
             var users = db.Admins.Where(u => u.Id.Equals(newAdmin.Id)).FirstOrDefault();
@@ -89,7 +83,7 @@ namespace Eproject3.Controllers
                 {
                     db.Admins.Add(newAdmin);
                     db.SaveChanges();
-                    return RedirectToAction("Index","Home");
+                    return RedirectToAction("Index", "Home");
                 }
                 else
                 {
@@ -102,7 +96,26 @@ namespace Eproject3.Controllers
             }
             return View();
         }
-        
+
+        private string GenerateJwtToken(Admin user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_config["Jwt:Key"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Audience = _config["Jwt:Audience"],
+                Issuer = _config["Jwt:Issuer"],
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.AdminName),
+                    new Claim(ClaimTypes.Role, user.Role)
+                }),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
 
     }
 }
--- Eproject3/Eproject3/Models/Admin.cs
+++ Eproject3/Eproject3/Models/Admin.cs
@@ -10,6 +10,6 @@ namespace Eproject3.Models
         public string Id { get; set; } = null!;
         public string AdminName { get; set; } = null!;
         public string Password { get; set; } = null!;
-        public string Role { get; set; } = null!;
+		public string Role { get; set; } = null!;
     }
 }
