c475b092b6504062ef3bbec323d1b993a25a8e60
Thuan412003 <89301691+Thuan412003@users.noreply.github.com>
2023-03-02 01:25:21 +0700

    commit

---
+++ Eproject3/Eproject3/Controllers/ComplainController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Eproject3.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+namespace Eproject3.Controllers
+{
+    public class ComplainController : Controller
+    {
+        private eProject3Context db;
+
+        public ComplainController(eProject3Context _db)
+        {
+            db = _db;
+        }
+        public IActionResult Index()
+        {
+            var result = db.Complains.Include(x => x.IdNavigation).ToList();
+            return View(result);
+        }
+        [HttpPost]
+        [ActionName("Create")]
+        public IActionResult Create(Complain newCPL)
+        {
+
+            //var user = db.Users.Where(u=>u.id.Equals(newUser.id)).FirstOrDefault();
+            Complain newComplain = new Complain();
+            newComplain.Description = newCPL.Description;
+            newComplain.Reason = newCPL.Reason;
+            newComplain.StatusCp = newCPL.StatusCp;
+            newComplain.DateCp = DateTime.Now;
+            newComplain.Category = newCPL.Category;
+            newComplain.Id = newCPL.Id;
+            db.Complains.Add(newComplain);
+            db.SaveChanges();
+            ViewData["Id"] = new SelectList(db.Admins, "Id", "AdminName", newCPL.Id);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewData["Id"] = new SelectList(db.Admins, "Id", "AdminName");
+            return View();
+        }
+        [HttpGet]
+        public IActionResult EditComplain(int id)
+        {
+            var model = db.Complains.Where(d => d.ComplainId.Equals(id)).FirstOrDefault();
+            ViewData["Id"] = new SelectList(db.Admins, "Id", "AdminName", model.Id);
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult EditComplain(Complain newCPL)
+        {
+            var model = db.Complains.Where(d => d.ComplainId.Equals(newCPL.ComplainId)).FirstOrDefault();
+
+            if (model != null)
+            {
+                model.Description = newCPL.Description;
+                model.Reason = newCPL.Reason;
+                model.StatusCp = newCPL.StatusCp;
+                model.DateCp = DateTime.Now;
+                model.Category = newCPL.Category;
+                model.Id = newCPL.Id;
+
+                db.Update(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "can not update Complain");
+            }
+            return View(newCPL);
+        }
+        [HttpGet]
+        public IActionResult DeleteComplain(int id)
+        {
+            var result = db.Complains.FirstOrDefault(d => d.ComplainId.Equals(id));
+            if (result != null)
+            {
+                db.Complains.Remove(result);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var result = db.Complains.Where(d => d.ComplainId.Equals(id)).FirstOrDefault();
+            return View(result);
+        }
+    }
+}
--- Eproject3/Eproject3/Controllers/HomeController.cs
+++ Eproject3/Eproject3/Controllers/HomeController.cs
@@ -28,8 +28,8 @@ namespace Eproject3.Controllers
         public IActionResult Index()
         {
             HttpContext.Session.GetString("adminId");
-            ViewData["Resources"] = JSONListHelper.GetResourceListJSONString(db.Labs.ToList());
-            ViewData["Events"] = JSONListHelper.GetEventListJSONString(db.Events.ToList());
+            //ViewData["Resources"] = JSONListHelper.GetResourceListJSONString(db.Labs.ToList());
+            //ViewData["Events"] = JSONListHelper.GetEventListJSONString(db.Events.ToList());
             return View();
         }
 
